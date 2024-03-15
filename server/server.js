94635b9f9574488ac3b7316c66beb48748a28679
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-03-15 18:05:54 -0300

    feature/ stop main process

--- server/server.js
+++ server/server.js
@@ -26,10 +26,10 @@ app.use('/binance', binanceRoutes);
 app.use('/database', databaseRoutes); 
 
 
-runCronTask("updateWrongData", updateWrongData, '*/2 * * * * *', 'America/Argentina/Buenos_Aires');
-runCronTask("updateMissingData", updateMissingData, '*/20 * * * * *', 'America/Argentina/Buenos_Aires');
-runCronTask("checkCompleteness", checkCompleteness, '*/10 * * * * *', 'America/Argentina/Buenos_Aires');
-runCronTask("cronController", cronController, '*/15 * * * *', 'America/Argentina/Buenos_Aires');
-runCronTask("updateAtr", updateAtr, '*/2 * * * *', 'America/Argentina/Buenos_Aires');
+//runCronTask("updateWrongData", updateWrongData, '*/2 * * * * *', 'America/Argentina/Buenos_Aires');
+//runCronTask("updateMissingData", updateMissingData, '*/20 * * * * *', 'America/Argentina/Buenos_Aires');
+//runCronTask("checkCompleteness", checkCompleteness, '*/10 * * * * *', 'America/Argentina/Buenos_Aires');
+//runCronTask("cronController", cronController, '*/15 * * * *', 'America/Argentina/Buenos_Aires');
+//runCronTask("updateAtr", updateAtr, '*/2 * * * *', 'America/Argentina/Buenos_Aires');
 
 updateTrending()
