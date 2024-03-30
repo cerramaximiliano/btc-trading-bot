8a439ca4b81788c75f73cb982fe49bfe804ccc9a
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-03-30 17:56:51 -0300

    feature/ pm2 econsystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+        {
+            name: 'btc-admin',
+            script: './server/server.js',
+            env: {
+                PORT: 3001
+            },
+            exec_mode: 'fork',
+            instance: 1,
+            kill_timeout: 4000,
+            listen_timeout: 5000,
+            wait_ready: true
+        }
+    ]
+}
