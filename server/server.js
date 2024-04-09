b5df9eaaa652183fabc9682943e8a2f734cbb35c
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-04-09 20:55:48 -0300

    fix/ cors policy

--- server/server.js
+++ server/server.js
@@ -10,15 +10,7 @@ const cookieParser = require('cookie-parser');
 
 const cors = require('cors');
 const allowedOrigins = ['http://localhost:3001','https://adminboards.app', 'https://www.adminboards.app'];
-app.use(cors({origin: function(origin, callback){
-if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-        const msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-    }
-        return callback(null, true);
-    }
-}));
+app.use(cors());
 
 const { logger } = require('./config/pino');
 const { runCronTask, agendash } = require('./utils/cronTasks');
